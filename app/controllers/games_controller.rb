a997251e574229fc7ef5ff38bc86bff24e856774
Josh Evans <josh944@gmail.com>
2013-10-05 16:38:34 -0700

    More changes between production and development

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,12 @@ class GamesController < ApplicationController
   def run
     if Rails.env.production?
       @app_id = "638116506222271"
+      @host = request.host
     else
       @app_id = "400254313434669"
+      @host= "localhost:3000"
     end
+    
+    
   end
 end
