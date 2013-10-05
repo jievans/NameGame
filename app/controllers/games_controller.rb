5a8f5388046e5c51fa6a041f43f97feb3ae8e0dc
Josh Evans <josh944@gmail.com>
2013-10-05 16:26:49 -0700

    Added logic for app key changing between production and development

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,9 @@
 class GamesController < ApplicationController
   def run
+    if Rails.env.production?
+      @app_id = "638116506222271"
+    else
+      @app_id = "400254313434669"
+    end
   end
 end
